eee4edbc0c3507276e41b11107786f51db4390f6
Daehan <dh16931@gmail.com>
2021-07-25 16:27:08 +0900

    feat: input for filtering verbs

--- components/Verblist.js
+++ components/Verblist.js
@@ -5,14 +5,19 @@ import useSelectItem from "../hooks/useSelectItem";
 
 import * as Data from "../data";
 
+const verbResources = Data.verbs;
+const particleResources = Data.particles;
+const verbList = Object.keys(verbResources);
+
 const VerbList = () => {
-  const verbs = useSelectItem(Object.keys(Data.verbs));
-  const particles = useSelectItem([...Data.particles]);
+  const verbs = useSelectItem(verbList);
+  const particles = useSelectItem([...particleResources]);
   const [displayData, setDisplayData] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     if (verbs.selectedItem) {
-      const curVerbParticles = Object.keys(Data.verbs[verbs.selectedItem]);
+      const curVerbParticles = Object.keys(verbResources[verbs.selectedItem]);
       particles.setItems(curVerbParticles);
     }
   }, [verbs.selectedItem]);
@@ -21,17 +26,26 @@ const VerbList = () => {
     if (verbs.selectedItem && particles.selectedItem) {
       const curVerb = verbs.selectedItem;
       const curParticle = particles.selectedItem;
-      setDisplayData(Data.verbs[curVerb][curParticle]);
+      setDisplayData(verbResources[curVerb][curParticle]);
     }
   }, [verbs.selectedItem, particles.selectedItem]);
 
+  useEffect(() => {
+    const filteredVerbs = verbList.filter((verb) => verb.includes(searchText));
+    verbs.setItems(filteredVerbs);
+  }, [searchText]);
+
   return (
     <>
+      <input
+        placeholder="Type verb"
+        className={styles.input}
+        type="text"
+        onChange={(e) => setSearchText(e.target.value)}
+      />
       <div className={styles.flex}>
-        {verbs.items && verbs.items.length > 0 && <SelectItem {...verbs} />}
-        {particles.items && particles.items.length > 0 && (
-          <SelectItem {...particles} />
-        )}
+        {<SelectItem {...verbs} />}
+        {<SelectItem {...particles} />}
       </div>
       {displayData && displayData.length > 0 && (
         <>
