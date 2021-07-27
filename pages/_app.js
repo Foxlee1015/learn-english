301d4767704ca64933f84d4f7bd9dbc565dec7f3
Daehan <dh16931@gmail.com>
2021-07-27 22:30:05 +0900

    bug: refresh page error

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,10 @@ import Layout from "../components/Layout";
 import AdminNav from "../components/admin/AdminNav";
 import "../styles/globals.css";
 
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
   const { asPath } = useRouter();
-  // const [currentUrl, setCurrentUrl] = useState("/");
-
-  // useEffect(() => {
-  //   console.log(router.asPath);
-  // }, [router.asPath]);
-
-  // useEffect(() => {
-  //   console.log(currentUrl);
-  //   console.log(currentUrl.includes("/admin"));
-  // }, [currentUrl]);
 
   return (
     <Layout>
