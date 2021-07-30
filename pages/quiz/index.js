7f2f9b0fa64ee8adcf64271db84528dfec42bd4b
Daehan <dh16931@gmail.com>
2021-07-31 08:06:18 +0900

    feat: quiz menu style and change link to route push

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,15 +1,25 @@
-import Link from "next/link";
 import quizStyle from "../../styles/pages/quiz/QuizHome.module.css";
+import { useRouter } from "next/router";
+
+const btnLinks = [
+  { url: "/quiz/phrasalVerbs", text: "Phrasal Verbs" },
+  { url: "/quiz/idioms", text: "Idioms" },
+];
 
 const Quiz = ({}) => {
+  const router = useRouter();
+
   return (
     <div className={quizStyle.container}>
-      <div className={quizStyle.linkBtn}>
-        <Link href="/quiz/phrasalVerbs">Phrasal Verbs</Link>
-      </div>
-      <div className={quizStyle.linkBtn}>
-        <Link href="/quiz/idioms">Idioms</Link>
-      </div>
+      {btnLinks.map((btnLink) => (
+        <button
+          type="button"
+          onClick={() => router.push(btnLink.url)}
+          className={quizStyle.linkBtn}
+        >
+          {btnLink.text}
+        </button>
+      ))}
     </div>
   );
 };
