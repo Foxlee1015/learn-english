d079ed4b18ec7baf3c2ac1a99b331848ad09406c
Daehan <dh16931@gmail.com>
2021-08-01 13:29:13 +0900

    signin page

--- pages/member/signin.js
+++ pages/member/signin.js
@@ -1,7 +1,43 @@
+import { useState, useEffect } from "react";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
 const Signin = ({}) => {
+  const [values, setValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const onFinish = (e) => {
+    console.log(values);
+  };
+
   return (
     <>
       <h2>Signin</h2>
+      <input
+        name="username"
+        placeholder="username"
+        type="text"
+        onChange={handleChange}
+      ></input>
+      <input
+        name="password"
+        placeholder="password"
+        type="password"
+        onChange={handleChange}
+      ></input>
+      <button type="button" onClick={(e) => onFinish(e)}>
+        Submit
+      </button>
     </>
   );
 };
