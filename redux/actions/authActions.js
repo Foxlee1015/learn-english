11f3b9daf53fd34ae5e9fc6ae161f43150548812
Daehan <dh16931@gmail.com>
2021-08-03 00:18:48 +0900

    feat: update auth with callbacks

--- redux/actions/authActions.js
+++ redux/actions/authActions.js
@@ -2,26 +2,39 @@ import { server } from "../../config";
 import { AUTHENTICATE, DEAUTHENTICATE } from "../actionTypes";
 import Cookies from "universal-cookie";
 
-export const authenticate = (user) => (dispatch) => {
-  fetch(`${server}/api/sessions/`, {
-    body: JSON.stringify(user),
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      if (response && response.result) {
-        const cookies = new Cookies();
-        cookies.set("EID_SES", response.result, {
-          path: "/",
-          maxAge: 60 * 60 * 24,
-        });
-        dispatch({ type: AUTHENTICATE });
-      }
-    });
-};
+export const authenticate =
+  (user, successCallback, failCallback) => (dispatch) => {
+    fetch(`${server}/api/sessions/`, {
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json();
+        } else if (res.status === 400) {
+          failCallback("Please check your username and password");
+        } else {
+          failCallback("Sever error");
+        }
+      })
+      .then((response) => {
+        if (response && response.result) {
+          const cookies = new Cookies();
+          cookies.set("EID_SES", response.result, {
+            path: "/",
+            maxAge: 60 * 60 * 24,
+          });
+          dispatch({ type: AUTHENTICATE });
+          successCallback();
+        }
+      })
+      .catch(() => {
+        failCallback("Sever error");
+      });
+  };
 
 export const reauthenticate = () => (dispatch) => {
   const cookies = new Cookies();
