285eeae1af6c2b5f0953393f56be7f711d25dc88
Daehan <dh16931@gmail.com>
2021-08-04 02:01:16 +0900

    bug: showing nav before auth check

--- components/layouts/AdminLayout.js
+++ components/layouts/AdminLayout.js
@@ -1,11 +1,13 @@
+import { useSelector } from "react-redux";
 import AdminNav from "../navs/AdminNav";
 import Footer from "../Footer";
 import adminLayoutstyles from "../../styles/components/AdminLayout.module.css";
 
 const AdminLayout = ({ children }) => {
+  const auth = useSelector((state) => state.auth);
   return (
     <>
-      <AdminNav />
+      {auth.loggedIn && <AdminNav />}
       <div className={adminLayoutstyles.container}>
         <main className={adminLayoutstyles.main}>{children}</main>
       </div>
