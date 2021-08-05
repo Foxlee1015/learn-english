57129402c1d848f737908de4c7c910c54cf5440c
Daehan <dh16931@gmail.com>
2021-08-06 07:53:15 +0900

    feat: inputcheckbox style

--- components/common/InputCheckbox.js
+++ components/common/InputCheckbox.js
@@ -1,16 +1,24 @@
+import inputCheckboxStyles from "../../styles/components/InputCheckbox.module.css";
+
 const InputCheckbox = ({
   label = "",
   checked = false,
   onChange = () => {},
 }) => {
   return (
-    <div>
+    <div className={inputCheckboxStyles.container}>
       <input
+        className={inputCheckboxStyles.input}
         type="checkbox"
         checked={checked}
         onChange={(e) => onChange(e.target.checked)}
       ></input>
-      <label onClick={() => onChange(!checked)}>{label}</label>
+      <label
+        onClick={() => onChange(!checked)}
+        className={inputCheckboxStyles.label}
+      >
+        {label}
+      </label>
     </div>
   );
 };
