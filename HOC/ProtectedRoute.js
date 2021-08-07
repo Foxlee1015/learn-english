00407573bccbd0ff7183d8113832b80336ececbd
Daehan <dh16931@gmail.com>
2021-08-07 19:23:26 +0900

    feat: auth loader

--- HOC/ProtectedRoute.js
+++ HOC/ProtectedRoute.js
@@ -1,46 +1,56 @@
 import { useRouter } from "next/router";
-import dynamic from "next/dynamic"
-import {useState, useEffect} from "react"
+import dynamic from "next/dynamic";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { reauthenticate } from "../redux/actions/authActions"
+import { reauthenticate } from "../redux/actions/authActions";
 
-const AuthLoadingComponents = dynamic(()=>import("../components/AuthLoading"))
+const AuthLoadingComponents = dynamic(() =>
+  import("../components/AuthLoading")
+);
 
 const ProtectedRoute = (ProtectedComponent) => {
   return (props) => {
-      const Router = useRouter();
-      const dispatch = useDispatch();
-      const auth = useSelector((state) => state.auth);
-      const [verified, setVerified] = useState(false);
+    const Router = useRouter();
+    const dispatch = useDispatch();
+    const auth = useSelector((state) => state.auth);
+    const [verified, setVerified] = useState(false);
 
-      useEffect(async () => {
-        if (auth.loggedIn) { // redux
-          setVerified(true)
-        } else {    // directly access to the page
-          dispatch(
-            reauthenticate(
-              ()=>setVerified(true),
-              ()=>Router.replace("/member/signin")))
-        }
-      }, []);
-
-      if (verified) {
-        if (Router.pathname.includes("admin")) {
-          if (auth.is_admin === 1) {
-            return <ProtectedComponent {...props} />
-          } else {
-            Router.replace("/403")
-          }
-        }
-        return <ProtectedComponent {...props} />
+    useEffect(async () => {
+      if (auth.loggedIn) {
+        // redux
+        setVerified(true);
+      } else {
+        // directly access to the page
+        dispatch(
+          reauthenticate(
+            () => setVerified(true),
+            () => Router.replace("/member/signin")
+          )
+        );
       }
+    }, []);
 
-      // authenticating
-      if (auth.loggedIn) { // loading indicator - redux store login state
-        return null;
+    if (verified) {
+      if (Router.pathname.includes("admin")) {
+        if (auth.is_admin === 1) {
+          return <ProtectedComponent {...props} />;
+        } else {
+          Router.replace("/403");
+        }
       }
-      return <AuthLoadingComponents />; // show only directly access to the pages // re-auth lading indicator
-  }
-}
+      if (Router.pathname.includes("account")) {
+        return <ProtectedComponent {...props} />;
+      }
+      return <AuthLoadingComponents />;
+    }
+
+    // authenticating
+    if (auth.loggedIn) {
+      // loading indicator - redux store login state
+      return null;
+    }
+    return <AuthLoadingComponents />; // show only directly access to the pages // re-auth lading indicator
+  };
+};
 
 export default ProtectedRoute;
--- components/AuthLoading.js
+++ components/AuthLoading.js
@@ -1,5 +1,23 @@
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+const styles = {
+  conatiner: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    padding: 20,
+  },
+};
+//
 const AuthLoading = () => {
-  return <h2>Authenticating...</h2>;
+  return (
+    <div style={styles.conatiner}>
+      <h2>Authenticating...</h2>
+      <div>
+        <PacmanLoader color="#0070f3" />
+      </div>
+    </div>
+  );
 };
 
 export default AuthLoading;
