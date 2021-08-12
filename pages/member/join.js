068d5bfc2eeabd64f5a9cee79a12159b1b6cf279
daehan <dh16931@gmail.com>
2021-08-12 11:56:13 +0900

    feat: validate passwords

--- pages/member/join.js
+++ pages/member/join.js
@@ -5,6 +5,25 @@ import useInput from "../../hooks/useInput";
 import signinStyles from "../../styles/components/Signin.module.css";
 import { postNewUser } from "../../utils/apis";
 
+function validatePassword(p, setErrors) {
+  const errors = [];
+  if (p.length < 8) {
+      errors.push("Your password must be at least 8 characters"); 
+  }
+  if (p.search(/[a-z]/i) < 0) {
+      errors.push("Your password must contain at least one letter.");
+  }
+  if (p.search(/[0-9]/) < 0) {
+      errors.push("Your password must contain at least one digit."); 
+  }
+  if (errors.length > 0) {
+    setErrors(errors.join("\n"))
+  } else {
+    setErrors("")
+  }
+}
+
+
 const Join = ({}) => {
   const router = useRouter();
   const username = useInput("username", "username", "text");
@@ -17,6 +36,7 @@ const Join = ({}) => {
   const [errMsg, setErrMsg] = useState("");
   const [openSubmit, setOpenSubmit] = useState(false);
 
+
   useEffect(() => {
     setOpenSubmit(false);
     if (username.value !== "" && password.value !== "") {
@@ -27,7 +47,7 @@ const Join = ({}) => {
   }, [username.value, password.value, passwordConfirm.value]);
 
   const submit = () => {
-    if (openSubmit) {
+    if (openSubmit && error === "") {
       postNewUser(
         {
           name: username.value,
@@ -46,15 +66,23 @@ const Join = ({}) => {
     }
   };
 
+  const validatePasswords = () => {
+    if (password.value !== passwordConfirm.value) {
+      setErrMsg("password and password confirm must match.")
+    }
+  }
+
   return (
     <>
       <h2>Join</h2>
       <input {...username} className={signinStyles.input}></input>
-      <input {...password} className={signinStyles.input}></input>
+      <input {...password} className={signinStyles.input} onBlur={()=>{
+    validatePassword(password.value, setErrMsg)}}></input>
       <input
         {...passwordConfirm}
         className={signinStyles.input}
         onKeyDown={handleKeyDown}
+        onBlur={()=>validatePasswords()}
       ></input>
       <button
         type="button"
