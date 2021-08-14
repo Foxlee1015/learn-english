de31f647ee0d168897ebcefe9cb5c54d9dc67d64
Daehan <dh16931@gmail.com>
2021-08-14 16:29:31 +0900

    feat: admin form and list

--- components/admin/PhrasalVerbList.js
+++ components/admin/PhrasalVerbList.js
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
+
 const PhrasalVerbList = () => {
-  return <>PhrasalVerbList</>;
+  const [fetchPhrasalVerbs, doFetchPhrasalVerbs] = useFetch([]);
+
+  useEffect(() => {
+    doFetchPhrasalVerbs("phrasal-verbs/");
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {fetchPhrasalVerbs.data &&
+        fetchPhrasalVerbs.data.length > 0 &&
+        fetchPhrasalVerbs.data.map((item) => (
+          <button
+            key={item._id}
+            style={{
+              margin: 5,
+              border: "solid 1px black",
+              padding: 5,
+              boxSizing: "border-box",
+            }}
+          >
+            {item.verb}
+          </button>
+        ))}
+    </div>
+  );
 };
 
 export default PhrasalVerbList;
