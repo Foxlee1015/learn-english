d6a203dc6667d711018606492fb718644002d38a
Daehan <dh16931@gmail.com>
2021-08-15 20:39:16 +0900

    feat: useformlist hook

--- pages/admin/phrasalVerbs.js
+++ pages/admin/phrasalVerbs.js
@@ -1,41 +1,16 @@
-import { useEffect, useState } from "react";
 import PhrasalVerbForm from "../../components/admin/PhrasalVerbForm";
 import PhrasalVerbList from "../../components/admin/PhrasalVerbList";
 
 import AdminStyle from "../../styles/pages/admin/Admin.module.css";
-import useFetch from "../../hooks/useFetch";
+import useFormList from "../../hooks/useFormList";
 
 const PhrasalVerbs = () => {
-  const [fetchPhrasalVerbs, doFetchPhrasalVerbs] = useFetch([]);
-  const [selectedItem, setSelectedItem] = useState({});
-
-  const loadPhrasalVerbs = () => {
-    setSelectedItem({});
-    doFetchPhrasalVerbs("phrasal-verbs/");
-  };
-
-  useEffect(() => {
-    loadPhrasalVerbs();
-  }, []);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [selectedItem]);
+  const phrasalVerbFormList = useFormList("phrasal-verbs/");
 
   return (
     <div className={AdminStyle.container}>
-      <PhrasalVerbForm
-        data={fetchPhrasalVerbs.data}
-        selectedItem={selectedItem}
-        setSelectedItem={setSelectedItem}
-        refreshData={loadPhrasalVerbs}
-      />
-      <PhrasalVerbList
-        data={fetchPhrasalVerbs.data}
-        selectedItem={selectedItem}
-        setSelectedItem={setSelectedItem}
-        refreshData={loadPhrasalVerbs}
-      />
+      <PhrasalVerbForm {...phrasalVerbFormList} />
+      <PhrasalVerbList {...phrasalVerbFormList} />
     </div>
   );
 };
