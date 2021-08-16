aeaa6157ac402affd0dd5f18fc01e827cedfc4a3
Daehan <dh16931@gmail.com>
2021-08-16 15:41:22 +0900

    feat: add mail button in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import footerStyles from "../styles/components/Footer.module.css";
+import MailtoButton from "./common/MailtoButton";
 
 const Footer = () => {
   return (
     <div className={footerStyles.container}>
+      <MailtoButton
+        label="Write me an Email If you have any questions"
+        mailto="mailto:daehanlee.dev@gmail.com"
+      />
       <div>
         {`©${new Date().getFullYear()} Copyright : `}
         <Link href={"/home"}>{process.env.APP_URL}</Link>
