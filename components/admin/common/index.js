7a5dc9040b31e349df76c4cd617dc1195175a554
Daehan <dh16931@gmail.com>
2021-08-29 17:03:03 +0900

    fix: typo in import component

--- components/admin/common/index.js
+++ components/admin/common/index.js
@@ -1,5 +1,5 @@
 import AntFormList from "./AntFormList";
-import AdminContainer from "./AdminContainer.";
+import AdminContainer from "./AdminContainer";
 import AdminContentListContainer from "./AdminContentListContainer";
 
 export { AntFormList, AdminContainer, AdminContentListContainer };
