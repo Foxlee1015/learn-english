e9d0eef9b7e8575d102656a1c758fac8c17ba070
daehan <dh16931@gmail.com>
2021-10-18 21:25:29 +0900

    fix: update api endpoint

--- pages/phrasalverbs/[verb].js
+++ pages/phrasalverbs/[verb].js
@@ -7,7 +7,7 @@ import { useState, useEffect } from "react";
 const Page = ({ phrasalVerbs }) => {
    const [selectedPhrasalVerb, setSelectedPhrasalVerb] = useState(null);
 
-   useEffect(()=>{
+   useEffect(() => {
       if (phrasalVerbs.length > 0) {
          setSelectedPhrasalVerb(phrasalVerbs[0])
       }
@@ -15,41 +15,41 @@ const Page = ({ phrasalVerbs }) => {
 
    return (<>
       <Meta title={`Learn English - phrasal verb ${phrasalVerbs[0].verb} ${phrasalVerbs[0].particle}`} />
-      {selectedPhrasalVerb  && (
+      {selectedPhrasalVerb && (
          <>
-         <h5>{phrasalVerbs[0].verb}</h5>
-         {phrasalVerbs.map((phrasalVerb) => (
-            <>
-               <span key={phrasalVerb.particle} onClick={()=>{setSelectedPhrasalVerb(phrasalVerb)}}>{phrasalVerb.particle}</span>
-            </>
-         ))}
-         {selectedPhrasalVerb && (
-            <div key={selectedPhrasalVerb.particle}>
-               <DescCard data={selectedPhrasalVerb.definitions} title={"Definition"} />
-               <DescCard data={selectedPhrasalVerb.sentences} title={"Examples"} />
-            </div>
-         )}
+            <h5>{phrasalVerbs[0].verb}</h5>
+            {phrasalVerbs.map((phrasalVerb) => (
+               <>
+                  <span key={phrasalVerb.particle} onClick={() => { setSelectedPhrasalVerb(phrasalVerb) }}>{phrasalVerb.particle}</span>
+               </>
+            ))}
+            {selectedPhrasalVerb && (
+               <div key={selectedPhrasalVerb.particle}>
+                  <DescCard data={selectedPhrasalVerb.definitions} title={"Definition"} />
+                  <DescCard data={selectedPhrasalVerb.sentences} title={"Examples"} />
+               </div>
+            )}
          </>
       )}
    </>);
 };
 
 export async function getStaticPaths() {
-   const res = await fetch(`${server}/api/phrasal-verbs/?only_verb=1`);
+   const res = await fetch(`${server}/api/verbs/`);
    const data = await res.json()
- 
+
    const paths = data.result.map((verb) => ({
-     params: { verb },
+      params: { verb },
    }))
- 
+
    return { paths, fallback: false }
- }
- 
+}
+
 export async function getStaticProps({ params }) {
    const res = await fetch(`${server}/api/phrasal-verbs/?search_key=${params.verb}&exact=1`);
    const data = await res.json()
-   
-   return { props: { phrasalVerbs : data.result } }
- }
- 
+
+   return { props: { phrasalVerbs: data.result } }
+}
+
 export default Page
