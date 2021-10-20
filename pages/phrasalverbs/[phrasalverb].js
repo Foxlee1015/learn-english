3588b1fed9c2f9cf5ac7e42377559354cf62938e
daehan <dh16931@gmail.com>
2021-10-20 10:00:42 +0900

    feat: update phrasalverb getstaticprops

---
+++ pages/phrasalverbs/[phrasalverb].js
@@ -0,0 +1,59 @@
+
+import { server } from "../../config";
+import { DescCard } from "../../components/common";
+import { Meta } from "../../components";
+import { useState, useEffect } from "react";
+
+const Page = ({ phrasalVerbs }) => {
+   const [selectedPhrasalVerb, setSelectedPhrasalVerb] = useState(null);
+
+   useEffect(() => {
+      if (phrasalVerbs.length > 0) {
+         setSelectedPhrasalVerb(phrasalVerbs[0])
+      }
+   }, [phrasalVerbs])
+
+   return (<>
+      <Meta title={`Learn English - phrasal verb ${phrasalVerbs[0].verb} ${phrasalVerbs[0].particle}`} />
+      {selectedPhrasalVerb && (
+         <>
+            <h5>{phrasalVerbs[0].verb}</h5>
+            {phrasalVerbs.map((phrasalVerb) => (
+               <>
+                  <span key={phrasalVerb.particle} onClick={() => { setSelectedPhrasalVerb(phrasalVerb) }}>{phrasalVerb.particle}</span>
+               </>
+            ))}
+            {selectedPhrasalVerb && (
+               <div key={selectedPhrasalVerb.particle}>
+                  <DescCard data={selectedPhrasalVerb.definitions} title={"Definition"} />
+                  <DescCard data={selectedPhrasalVerb.sentences} title={"Examples"} />
+               </div>
+            )}
+         </>
+      )}
+   </>);
+};
+
+export async function getStaticPaths() {
+   const res = await fetch(`${server}/api/phrasal-verbs/`);
+   const data = await res.json()
+
+   const paths = data.result.map(({ verb, particle }) => {
+      const phrasalverb = `${verb}-${particle.replace(" ", "-")}`
+      return ({
+         params: { phrasalverb },
+      })
+   }
+   )
+
+   return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+   const res = await fetch(`${server}/api/phrasal-verbs/${params.phrasalverb}`);
+   const data = await res.json()
+
+   return { props: { phrasalVerbs: data.result } }
+}
+
+export default Page
